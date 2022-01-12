7d977837221e9ed18d76decbd2c5e79f935e80ad
AHHatton <amanda.h.hatton@gmail.com>
2022-01-11 19:20:28 -0900

    Part 2 of project- validation of input

--- js/script.js
+++ js/script.js
@@ -7,6 +7,9 @@ const remainGuessSpan = document.querySelector(".remaining span"); //paragraph s
 const message = document.querySelector(".message"); //paragraph for messages
 const playAgainButton = document.querySelector(".play-again"); // play again button (hidden)
 const word = "magnolia";
+const guessedLetters = [];
+
+
 
 const placeholderCircle = function(word) {
   const numOfLetters = [];
@@ -23,7 +26,34 @@ placeholderCircle(word);
 
 guessButton.addEventListener("click", function(e){
  e.preventDefault();
+ message.innerText= "";
  const inputLetter = letterInputBox.value;
- console.log(inputLetter);
- letterInputBox.value = "";
+ const validatedInput = playerInput(inputLetter);
+ if (validatedInput) { //I think this checks to see if anything has been returned, i.e. if input passed validation and was passed to this variable?
+ 	makeGuess(inputLetter);}
+letterInputBox.value = "";
+
 });
+
+const playerInput = function(input) { 
+ const acceptedLetter = /[a-zA-Z]/;
+ if (input.length === 0) {
+ 	message.innerText = "Please input a letter";
+    } else if (input.length >1) {
+    	message.innerText = "Please only put in one letter.";
+    } else if (!input.match(acceptedLetter)) {
+    	message.innerText = "Only letters are allowed.";
+    } else {return input;
+    }
+    
+};
+
+const makeGuess = function(inputLetter) {
+ inputLetter = inputLetter.toUpperCase();
+  if (guessedLetters.includes(inputLetter)) {
+  	message.innerText = "This letter has already been guessed, please try a different letter";
+ } else {
+ 	guessedLetters.push(inputLetter);
+    console.log(guessedLetters); 
+}};
+
